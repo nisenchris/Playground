12de2ca3134897b45dfea1ca7a298eed87a7eaab
nisenchris <c.nisen@gmail.com>
2024-05-21 11:11:00 -0500

    added Breadcrumb in dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -10,6 +10,12 @@ import {
   Text,
   useColorModeValue,
   ChakraProvider,
+  Portal,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  Link,
 } from "@chakra-ui/react";
 // things I added // Kika
 import MiniStatistics from "component/MiniStatistics";
@@ -35,7 +41,19 @@ import SalesOverview from "views/Dashboard/Dashboard/components/SalesOverview";
 import { useFlags } from "launchdarkly-react-client-sdk";
 
 const Dashboard = ({ user, setUser }) => {
-  const { devUser } = useFlags();
+  const { lineChart } = useFlags();
+
+  // Here are all the props that may change depending on navbar's type or state.(secondary, variant, scrolled)
+  let mainText = useColorModeValue("gray.700", "gray.200");
+  let secondaryText = useColorModeValue("gray.400", "gray.200");
+  let navbarPosition = "absolute";
+  let navbarFilter = "none";
+  let navbarBackdrop = "blur(21px)";
+  let navbarShadow = "none";
+  let navbarBg = "none";
+  let navbarBorder = "transparent";
+  let secondaryMargin = "0px";
+  let paddingX = "15px";
 
   const history = useHistory();
   const iconBoxInside = useColorModeValue("white", "white");
@@ -50,20 +68,28 @@ const Dashboard = ({ user, setUser }) => {
       <MainPanel
         w={{
           base: "100%",
-          xl: "calc(100% )",
+          xl: "calc(100%)",
         }}
       >
-        <Flex flexDirection="column" pt={{ base: "120px", md: "75px" }}>
-          <Box textAlign="center">
-            <Heading>Dashboard</Heading>
-            <Text mt={4}>Welcome, {user}!</Text>
-            <Button mt={4} onClick={handleSignOut}>
-              Sign Out
-            </Button>
-          </Box>
-          <PanelContent w="75%">
-            <PanelContainer>
-              <SimpleGrid columns={{ sm: 1, md: 2, xl: 4 }} spacing="24px">
+        <Flex flexDirection="column" pt={{ base: "120px", md: "75px" }} bg="gray.200" >
+          <PanelContent w="75%" >
+            <PanelContainer >
+              <Box textAlign="right"  >
+                <Button mt={4} onClick={handleSignOut} bg="gray.400">
+                  Sign Out
+                </Button>
+                <Text fontWeight="bold" mt={4}>{user}</Text>
+              </Box>
+
+                <Box mb={{ sm: "8px", md: "0px" }} >
+                  <Breadcrumb>
+                    <BreadcrumbItem color={mainText} fontWeight="bold">
+                      <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
+                    </BreadcrumbItem>
+                  </Breadcrumb>
+                </Box>
+
+              <SimpleGrid columns={{ sm: 1, md: 2, xl: 4 }} spacing="24px" my='26px'>
                 <MiniStatistics
                   title={"Today's Moneys"}
                   amount={"$53,000"}
@@ -108,12 +134,15 @@ const Dashboard = ({ user, setUser }) => {
                   percentage={23}
                   chart={<BarChart />}
                 />
-                { devUser ? (
-                <SalesOverview
-                  title={"Sales Overview"}
-                  percentage={25}
-                  chart={<LineChart />}
-                /> ) : <div />}
+                {lineChart ? (
+                  <SalesOverview
+                    title={"Sales Overview"}
+                    percentage={25}
+                    chart={<LineChart />}
+                  />
+                ) : (
+                  <div />
+                )}
               </Grid>
             </PanelContainer>
           </PanelContent>
