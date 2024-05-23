9cb3c8eed56fce64230cd3f0c18767bd498004c2
nisenchris <c.nisen@gmail.com>
2024-05-23 18:17:31 -0500

    AdminNavbarLinks.js cleanup

--- src/components/Navbars/AdminNavbarLinks.js
+++ src/components/Navbars/AdminNavbarLinks.js
@@ -1,5 +1,5 @@
 // Chakra Icons
-import { BellIcon, SearchIcon } from "@chakra-ui/icons";
+import { SearchIcon } from "@chakra-ui/icons";
 // Chakra Imports
 import {
   Button,
@@ -8,17 +8,9 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
   Text,
   useColorModeValue,
 } from "@chakra-ui/react";
-// Assets
-import avatar1 from "assets/img/avatars/avatar1.png";
-import avatar2 from "assets/img/avatars/avatar2.png";
-import avatar3 from "assets/img/avatars/avatar3.png";
 // Custom Icons
 import {
   ProfileIcon,
@@ -26,12 +18,9 @@ import {
   LaunchDarklyIcon,
 } from "components/Icons/Icons";
 // Custom Components
-import { ItemContent } from "components/Menu/ItemContent";
-import SidebarResponsive from "components/Sidebar/SidebarResponsive";
 import PropTypes from "prop-types";
 import React from "react";
 import { NavLink } from "react-router-dom";
-import routes from "routes.js";
 
 export default function HeaderLinks(props) {
   const { variant, children, fixed, secondary, onOpen, ...rest } = props;
